9e91884081dfdcb02a45ed86b9661614c5eb34ce
Avinay504 <avinay504@gmail.com>
2016-10-26 15:23:26 +0100

    needed modification in product.java and user.java.
    While creating teat class we have to add setter&getters class in every classtest file.

---
+++ Category.java
@@ -0,0 +1,35 @@
+package com.aj.james.backendproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+
+@Entity
+@Table(name="CATEGORY")
+@Component
+public class Category {
+	@Id
+	private int category_id;
+	private String category_name;
+	
+	
+	public int getCategory_id() {
+		return category_id;
+	}
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+	public String getCategory_name() {
+		return category_name;
+	}
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+	
+
+
+}
---
+++ CategoryTest.java
@@ -0,0 +1,24 @@
+package com.aj.james.backendproject;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.aj.james.backendproject.config.ApplicationConfig;
+import com.aj.james.backendproject.model.Category;
+
+public class CategoryTest {
+
+	public static void main(String[] args) {
+			
+		AnnotationConfigApplicationContext annotationApplication = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+		System.out.println("AppcOntext");
+		
+		annotationApplication.scan("com.aj.james.backendproject");
+
+		System.out.println("refresh");
+		
+		Category category =  (Category) annotationApplication.getBean("category");
+		System.out.println(""+category);
+		System.out.println("category");
+	}
+
+}
